ab1ce322bdc8833fe4b5848b6a258aec98411dbf
Md. Sadaf Noor <sadaf2605@gmail.com>
2012-12-30 21:12:20 +0600

    Update source/inject.js

--- source/inject.js
+++ source/inject.js
@@ -17,7 +17,7 @@ var myVar=setInterval(function(){doit()},10000);
 
 function doit(){
 //    alert("doing it");
-    ReplaceContentInContainer("ego_section", "We have removed all the annoying people you may know and advertice of facebook as much as we can.\nIf you still face a problem then let us know.");
+    ReplaceContentInContainer("ego_section", "We have removed all the annoying people you may know and advertice of facebook as much as we can.\nIf you still face a problem <a href='http://sadaf2605.appspot.com/blog/fbPUMS:%20Irritating%20People%20you%20may%20know%20and%20Ad.%20hider'>then let us know.</a>");
 //    alert(""+con+doingcount);
     
 }
